816a28a832f44c82b27a898f3c4b90eb6d94052a
zliang12 <zliang12@ualberta.ca>
2025-10-28 06:43:50 -0600

    finished profile_screen

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 dependencies {
 
     implementation(libs.appcompat)
-    implementation("com.google.android.material:material:1.12.0")
+    implementation(libs.material.v1120)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
