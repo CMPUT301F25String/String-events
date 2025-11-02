ea651ccbdc4fc4c8ebe0ca1e60cc113c89bdf555
zliang12 <zliang12@ualberta.ca>
2025-11-02 09:57:32 -0700

    Started work on user creating events
    US 02.01.04, US 02.01.01, US 02.03.01 , US 02.04.01, US 02.04.02, US 02.05.02, US 02.02.03

--- code/app/src/main/java/com/example/string_events/MainActivity.java
+++ code/app/src/main/java/com/example/string_events/MainActivity.java
@@ -4,14 +4,12 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.string_events.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.events_screen);
     }
 }
